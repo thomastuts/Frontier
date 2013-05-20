a94a4f35180b0d6578861ad1b11219f70700110a
Thomas Tuts <thomas.tuts@gmail.com>
2013-05-20 14:52:53 +0200

    Implement basic view switching functionality with ngInclude

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,23 @@
 'use strict';
 
 angular.module('frontierApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .factory('Data', function () {
+      return { message: "I'm data from a service"}
+    })
+    .directive("enter", function () {
+      return function (scope, element) {
+        element.bind('mouseenter', function () {
+          console.log("I am inside of you");
+        })
+      }
+    })
+    .config(function ($routeProvider) {
+      $routeProvider
+          .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+          })
+          .otherwise({
+            redirectTo: '/'
+          });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('frontierApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, Data) {
+      $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+
+      $scope.partialUrl = 'views/detail.html';
+
+    });

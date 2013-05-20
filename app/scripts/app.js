2806200409a826162b5723ddd292b2fe54dbd8c2
Thomas Tuts <thomas.tuts@gmail.com>
2013-05-20 15:24:55 +0200

    Removed testing code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,6 @@
 'use strict';
 
 angular.module('frontierApp', [])
-    .factory('Data', function () {
-      return { message: "I'm data from a service"}
-    })
-    .directive("enter", function () {
-      return function (scope, element) {
-        element.bind('mouseenter', function () {
-          console.log("I am inside of you");
-        })
-      }
-    })
     .config(function ($routeProvider) {
       $routeProvider
           .when('/', {

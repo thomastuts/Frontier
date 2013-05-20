f2d703c65c33e98f47b79ae7f252d881b54c968f
Thomas Tuts <thomas.tuts@gmail.com>
2013-05-20 15:16:06 +0200

    Extra view functionality stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,35 @@
 
 angular.module('frontierApp')
     .controller('MainCtrl', function ($scope, Data) {
-      $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
+      // set default view
+      $scope.view = 'views/modules/fruit/overview.html';
+
+      $scope.fruits = [
+        {
+          name: 'Banana',
+          info: "It's yellow and curved."
+        },
+        {
+          name: 'Orange',
+          info: 'It is orange and zesty.'
+        },
+        {
+          name: 'Apple',
+          info: "It's green and can be pretty sour."
+        }
       ];
 
-      $scope.partialUrl = 'views/detail.html';
+      $scope.testje = function () {
+        console.log("Testje yoyo");
+      };
+
+      $scope.showDetail = function (fruit) {
+        $scope.fruit = fruit;
+        console.log($scope.fruit);
+        $scope.view = 'views/modules/fruit/detail.html';
+        console.log($scope.view);
+        $scope.testje = 'wutwut';
+      };
+
 
     });

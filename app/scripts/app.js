a3f54ea3aa52a9aeae09fbb2032d98f43155bcdb
Thomas Tuts <thomas.tuts@gmail.com>
2013-05-21 21:32:56 +0200

    Add basic Fruit module with multiple views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,8 @@ angular.module('frontierApp', [])
     .config(function ($routeProvider) {
       $routeProvider
           .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
+            templateUrl: 'views/dashboard.html',
+            controller: 'DashboardCtrl'
           })
           .otherwise({
             redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,39 +1,6 @@
 'use strict';
 
 angular.module('frontierApp')
-    .controller('MainCtrl', function ($scope, Data) {
-      // set default view
-      $scope.view = 'views/modules/fruit/overview.html';
+  .controller('DashboardCtrl', function ($scope) {
 
-      $scope.fruits = [
-        {
-          name: 'Banana',
-          info: "It's yellow and curved."
-        },
-        {
-          name: 'Orange',
-          info: 'It is orange and zesty.'
-        },
-        {
-          name: 'Apple',
-          info: "It's green and can be pretty sour."
-        }
-      ];
-
-      $scope.testje = function () {
-        console.log("Testje yoyo");
-      };
-
-      $scope.showDetail = function (fruit) {
-        $scope.fruit = fruit;
-        $scope.views = {
-          detail: {
-            result: _.find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; })
-          }
-        };
-        $scope.view = 'views/modules/fruit/detail.html';
-        $scope.testje = 'wutwut';
-      };
-
-
-    });
+  });

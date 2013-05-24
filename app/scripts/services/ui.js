4354e8f5b3841687cfdb8209383e3b261b3e10c5
Thomas Tuts <thomas.tuts@gmail.com>
2013-05-25 01:37:02 +0200

    Change minimize/maximize icon when toggling size

--- app/scripts/services/ui.js
+++ app/scripts/services/ui.js
@@ -5,19 +5,26 @@ angular.module('frontierApp')
     return {
       toggle: function (toggledModule) {
         var content = '.' + toggledModule.meta.name + ' .content';
-        $(content).toggle();
 
         // todo: implement animation so we don't this ugly toggle
         var name = toggledModule.meta.name;
         var open = toggledModule.ui.open;
+        var $toggle = $('.' + name + ' .toggle');
+
+        console.log(open);
 
         if(open === true) {
-          console.warn('Minimizing ' + toggledModule.meta.name);
+          console.warn('Minimizing...');
+          $toggle.removeClass('icon-chevron-up');
+          $toggle.addClass('icon-chevron-down');
         }
         else {
-          console.warn('Maximizing ' + toggledModule.meta.name);
+          console.warn('Maximizing...');
+          $toggle.removeClass('icon-chevron-down');
+          $toggle.addClass('icon-chevron-up');
         }
         toggledModule.ui.open = !open;
+        $(content).toggle();
       }
     };
   });

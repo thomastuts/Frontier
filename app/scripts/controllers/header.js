d61bc945892c9de5ed056a7e8a8c9efdfb2f19f2
Thomas Tuts <thomas.tuts@gmail.com>
2013-05-25 01:24:29 +0200

    Add a clock showing date & time to header

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -4,5 +4,12 @@ angular.module('frontierApp')
   .controller('HeaderCtrl', function ($scope, storageService) {
 
     $scope.data = storageService.get();
+    $scope.timer = moment().format($scope.data.config.general.datetime_format);
+
+    $scope.interval = window.setInterval(function () {
+      $scope.$apply(function () {
+        $scope.timer = moment().format($scope.data.config.general.datetime_format);
+      });
+    }, 1000);
 
   });

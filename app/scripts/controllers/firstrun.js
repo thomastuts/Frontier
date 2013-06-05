651b730408f315c4837392f9a122d347926f6a8d
Thomas Tuts <thomas.tuts@gmail.com>
2013-06-05 23:22:32 +0200

    Basic first run setup

--- app/scripts/controllers/firstrun.js
+++ app/scripts/controllers/firstrun.js
@@ -1,11 +1,33 @@
 'use strict';
 
 angular.module('frontierApp')
-  .controller('FirstRunCtrl', function ($scope) {
+  .controller('FirstRunCtrl', function ($scope, $location, storage) {
 
     $scope.config = {
-      username: '',
-      datetime_format: 'dddd DD/MM | H:mm:ss'
+      general: {
+        username: '',
+        datetime_format: 'dddd DD/MM | H:mm:ss'
+      }
+    };
+
+    $scope.github = {
+      username: ''
+    };
+
+    $scope.saveConfiguration = function () {
+      console.log($scope.config);
+      console.log($scope.github);
+
+      storage.set('config', $scope.config);
+      storage.set('module-github', $scope.github);
+
+      // instantiate empty containers
+      storage.set('module-api', {collections: []});
+      storage.set('module-inspiration', {sets: []});
+      storage.set('module-reminders', {reminders: []});
+      storage.set('module-todos', {projects: []});
+
+      $location.path('/');
     };
 
   });

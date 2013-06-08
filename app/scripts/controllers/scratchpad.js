0c4ad09c06ccf9d75f8605ff72a27d73f56fb474
Thomas Tuts <thomas.tuts@gmail.com>
2013-06-08 22:52:56 +0200

    Ability to add new snippet & limit overview snippets to last 5

--- app/scripts/controllers/scratchpad.js
+++ app/scripts/controllers/scratchpad.js
@@ -4,8 +4,13 @@ angular.module('frontierApp')
   .controller('ScratchpadCtrl', function ($scope, storage, viewer, ui) {
 
     $scope.data = {
-      overview: {},
-      repo: {}
+      overview: storage.get('module-scratchpad'),
+      new: {
+        name: '',
+        description: '',
+        date_created: '',
+        content: ''
+      }
     };
 
     $scope.module = {
@@ -21,10 +26,12 @@ angular.module('frontierApp')
         open: true // true for full window, false for minimized version
       },
       views: {
-        currentView: 'views/modules/scratchpad/scratchpad.html',
+        currentView: 'views/modules/scratchpad/overview.html',
         history: []
       }
     };
+
+    console.log($scope.data.overview);
 
     $scope.goBack = function () {
       viewer.goBack($scope);
@@ -36,6 +43,36 @@ angular.module('frontierApp')
 
     $scope.toggle = function () {
       ui.toggle($scope.module);
+    };
+
+    $scope.showNew = function () {
+      viewer.goToView($scope, 'views/modules/scratchpad/new.html', 'new');
+    };
+
+    $scope.addNewSnippet = function () {
+      var snippet = $('#scratchpad-editor').val();
+      console.log(snippet);
+
+      var snippetId;
+
+      if ($scope.data.overview.snippets.length === 0) {
+        snippetId = 1;
+      }
+      else {
+        snippetId = ($scope.data.overview.snippets[$scope.data.overview.snippets.length - 1].id) + 1;
+      }
+
+      $scope.data.new.id = snippetId;
+
+      $scope.data.new.content = snippet;
+      $scope.data.new.created_at = moment().format();
+
+
+      $scope.data.overview.snippets.push($scope.data.new);
+      storage.set('module-scratchpad', $scope.data.overview);
+      console.log($scope.data.new);
+      viewer.goToView($scope, 'views/modules/scratchpad/overview.html', 'new');
+
     };
 
 
